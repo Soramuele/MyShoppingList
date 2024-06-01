e14dd0e2c5151d9a3e930ca8cf510f8bb81ba4df
Samuele Catania <samuele.catania@hotmail.com>
2024-06-01 02:04:42 +0200

    Tryhard

--- Lucky five project/Assets/Scripts/Gyroscope.cs
+++ Lucky five project/Assets/Scripts/Gyroscope.cs
@@ -28,11 +28,25 @@ public class Gyroscope : MonoBehaviour
         Quaternion gyroQuaternion = GyroToUnity(Input.gyro.attitude);
         // rotate coordinate system 90 degrees. Correction Quaternion has to come first
         Quaternion calculatedRotation = correctionQuaternion * gyroQuaternion;
-        transform.rotation = calculatedRotation;
+        transform.rotation = new Quaternion(0, calculatedRotation.y, 0, calculatedRotation.w);
     }
 
     private static Quaternion GyroToUnity(Quaternion q)
     {
         return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+
+    void OnGUI()
+    {
+        GUIStyle labelStyle = new GUIStyle()
+        {
+            fontSize = 24,
+            normal = new GUIStyleState()
+            {
+                textColor = Color.black
+            }
+        };
+
+        GUI.Label(new Rect(10, 100, 500, 20), $"{Input.gyro.attitude.x}, {Input.gyro.attitude.y}, {Input.gyro.attitude.z}, {Input.gyro.attitude.w}", labelStyle);
     }
 }
