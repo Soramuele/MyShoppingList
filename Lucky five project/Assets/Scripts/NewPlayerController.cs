45fa5ad1edcd45130c395256f637d3b76c52937e
Samuele Catania <samuele.catania@hotmail.com>
2024-06-10 13:32:30 +0200

    minor changes

--- Lucky five project/Assets/Scripts/NewPlayerController.cs
+++ Lucky five project/Assets/Scripts/NewPlayerController.cs
@@ -6,7 +6,6 @@ public class NewPlayerController : MonoBehaviour
     private GameInput playerInput;
     private CharacterController controller;
     [SerializeField][Range(0.0f, 10.0f)] private float playerSpeed = 3f;
-    [SerializeField] private NewGyroscope cameraDir;
 
     // Awake is called when the script instance is being loaded.
     void Awake()
@@ -31,10 +30,9 @@ public class NewPlayerController : MonoBehaviour
         // Move the player
         Vector2 movementInput = playerInput.Player.Move.ReadValue<Vector2>();
         Vector3 move = new Vector3(movementInput.x, 0f, movementInput.y);
-        Vector3 movement = (cameraDir.virtualCamera.transform.forward * movementInput.y + cameraDir.virtualCamera.transform.right * movementInput.x).normalized;
         controller.Move(move * Time.deltaTime * playerSpeed);
 
         if (move != Vector3.zero)
-            transform.forward = movement;
+            transform.forward = move;
     }
 }
